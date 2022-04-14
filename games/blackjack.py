5bd057866a088a247aabecf8c20f3ae80fef018b
Jackson <66995801+jckirton@users.noreply.github.com>
2022-04-14 12:35:58 +0930

    Fixed game not adjusting for aces at the start

--- games/blackjack.py
+++ games/blackjack.py
@@ -342,10 +342,12 @@ def play():
         player_hand = Hand()
         player_hand.add_card(deck.deal())
         player_hand.add_card(deck.deal())
+        player_hand.adjust_for_ace()
 
         dealer_hand = Hand()
         dealer_hand.add_card(deck.deal())
         dealer_hand.add_card(deck.deal())
+        dealer_hand.adjust_for_ace()
 
         # Set up the Player's chips
         player_chips = Chips()  # remember the default value is 100
