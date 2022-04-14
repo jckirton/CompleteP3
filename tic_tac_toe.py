5d48a3fe796ca6cbb1d717d3795c0aa165899046
Jackson <66995801+jckirton@users.noreply.github.com>
2022-04-14 11:52:18 +0930

    Add files via upload

---
+++ tic_tac_toe.py
@@ -0,0 +1,231 @@
+"""
+Creates a playable tic tac toe game.
+"""
+import random
+import time
+from funcs import TerminalColors
+
+
+def display_board(board):
+    """
+    Creates and displays a tic tac toe board.
+    """
+    print("\n" * 100)
+    print(
+        "7    |8    |9   "
+    )
+    print("  " + board[7] + "  |  " + board[8] + "  |  " + board[9] + " ")
+    print("     |     |    ")
+    print("-----|-----|-----")
+    print(
+        "4    |5    |6  "
+    )
+    print("  " + board[4] + "  |  " + board[5] + "  |  " + board[6] + " ")
+    print("     |     |    ")
+    print("-----|-----|-----")
+    print(
+        "1    |2    |3  "
+    )
+    print("  " + board[1] + "  |  " + board[2] + "  |  " + board[3] + " ")
+    print("     |     |    ")
+
+
+def player_input():
+    """
+    Allows player 1 to choose wether to have X or O as their piece on the board.
+    """
+    marker = ""
+
+    while marker != "X" and marker != "O":
+        marker = input("Player 1 please choose X or O: ").upper()
+
+        player1 = (f"{marker}", 1)
+
+        if "X" in player1[0]:
+            player2 = ("O", 2)
+        else:
+            player2 = ("X", 2)
+
+    print(f"Player 1 is {player1[0]} and Player 2 is {player2[0]}")
+    return player1, player2
+
+
+def place_marker(board, marker, position):
+    """
+    Places the player's piece on the board.
+    """
+    board[position] = marker
+
+
+def win_check(board, mark):
+    """
+    Checks if a player has won or not.
+    """
+    if board[7] == board[8] == board[9] == mark:
+        return True
+
+    if board[1] == board[2] == board[3] == mark:
+        return True
+
+    if board[4] == board[5] == board[6] == mark:
+        return True
+
+    if board[7] == board[4] == board[1] == mark:
+        return True
+
+    if board[8] == board[5] == board[2] == mark:
+        return True
+
+    if board[9] == board[6] == board[3] == mark:
+        return True
+
+    if board[7] == board[5] == board[3] == mark:
+        return True
+
+    if board[9] == board[5] == board[1] == mark:
+        return True
+
+    else:
+        return False
+
+
+def choose_first():
+    """
+    Chooses which player goes first.
+    """
+    if random.randint(0, 1):
+        return True
+    else:
+        return False
+
+    # return True if random.randint(0, 1) else False
+
+
+def space_check(board, position):
+    """
+    Checks if the space that the player is trying to put his or her marker is free.
+    """
+    if "X" not in board[position] and "O" not in board[position]:
+        return True
+    else:
+        print("Space taken. Choose another")
+        return False
+
+
+def full_board_check(board):
+    """
+    Checks if the board is full.
+    """
+    for index in range(1, len(board)):
+        if "X" not in board[index] and "O" not in board[index]:
+            return False
+
+    return True
+
+
+def player_choice(board, player):
+    """
+    Asks the player where they would like to place there marker.
+    """
+    position = None
+    while position not in [1 - 9]:
+        try:
+            position = int(input(f"Player {player[1]} choose a position from 1-9: "))
+            if space_check(board, position):
+                return position
+        except ValueError:
+            print("please choose a number")
+        except IndexError:
+            print("please choose only 1-9")
+
+
+def replay():
+    """
+    Restarts the game.
+    """
+    while True:
+        check = input(
+            'Would you like to play again?\nType "maybe" or "?" to leave it up to the computer. '
+        ).lower()
+        if check in "no":
+            return False
+        elif check in "yes":
+            print("\n" * 100)
+            return True
+        elif check in "maybe?":
+            return random.randint(0, 1)
+        else:
+            print("yes or no?")
+            time.sleep(1)
+
+
+def loading():
+    """
+    Creates a false loading screen.
+    """
+    wait = random.randint(1, 10)
+    print("\nLoading Now")
+    print(f"Estimated Wait: {wait} second{'' if wait == 1 else 's'}")
+    time.sleep(wait)
+
+
+def play():
+    """
+    Starts the tic tac toe game.
+    """
+    while True:
+        print("\n" * 100)
+        print("Welcome to tic tac toe!")
+        print("A production by Ben and son, a coding family.")
+        # default board
+        board = ["#"] + [" "] * 9
+        # alternate board
+        # board = ["#"]
+        # for index in range(1, 10):
+        #     board.append(str(index))
+
+        player1, player2 = player_input()
+
+        game_on = True
+
+        loading()
+
+        while game_on:
+
+            marker = player1[0]
+            display_board(board)
+            position = player_choice(board, player1)
+
+            if space_check(board, position):
+                place_marker(board, marker, position)
+            display_board(board)
+
+            if win_check(board, marker):
+                print("Player 1 wins!!!")
+                break
+            if full_board_check(board):
+                print("the board is full. Nobody won.")
+                break
+            marker = player2[0]
+            position = player_choice(board, player2)
+
+            if space_check(board, position):
+                place_marker(board, marker, position)
+            display_board(board)
+
+            if win_check(board, marker):
+                print("Player 2 wins!!!")
+                break
+            if full_board_check(board):
+                print("the board is full. nobody won.")
+                break
+
+        if not replay():
+            print("Thanks for playing!")
+            time.sleep(1.5)
+            print("\n" * 100)
+            break
+
+
+if __name__ == "__main__":
+    play()
