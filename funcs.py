0962c42b3a5cb74bc0e9b74bcd9af8858c568258
Jackson <66995801+jckirton@users.noreply.github.com>
2022-04-14 12:05:31 +0930

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -42,22 +42,6 @@ def replay():
             time.sleep(1)
 
 
-class TerminalColors:
-    """
-    A variety of colors to use for your terminal.
-    """
-
-    grey = "\033[1;30;40m"
-    red = "\033[1;31;40m"
-    green = "\033[1;32;40m"
-    yellow = "\033[1;33;40m"
-    blue = "\033[1;34;40m"
-    magenta = "\033[1;35;40m"
-    cyan = "\033[1;36;40m"
-    white = "\033[1;37;40m"
-    regular = "\033[1;0;40m"
-
-
 def func_breaker(og_func):
     """
     A function that breaks other functions, good for if there under
