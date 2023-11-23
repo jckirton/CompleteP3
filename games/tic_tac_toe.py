0356375d523ee072155a31f2d65c70e63814ec70
Jackson <66995801+jckirton@users.noreply.github.com>
2023-11-23 11:40:00 +1030

    Update tic_tac_toe.py

--- games/tic_tac_toe.py
+++ games/tic_tac_toe.py
@@ -32,16 +32,16 @@ def player_input():
     while marker != "X" and marker != "O":
         marker = input("Player 1 please choose X or O: ").upper()
 
-        player1 = (f"{marker}", 1)
+        player1 = marker
 
-        if player1[0] == "X":
-            player2 = ("O", 2)
-        elif player1[0] == "O":
-            player2 = ("X", 2)
+        if player1 == "X":
+            player2 = "O"
+        elif player1 == "O":
+            player2 = "X"
         else:
             print("\n" * 100)
 
-    print(f"Player 1 is {player1[0]} and Player 2 is {player2[0]}")
+    print(f"Player 1 is {player1} and Player 2 is {player2}.")
     return player1, player2
 
 
@@ -91,7 +91,6 @@ def space_check(board, position):
     if (
         "X" not in board[position]
         and "O" not in board[position]
-        and "#" not in board[position]
     ):
         return True
     else:
@@ -199,7 +198,7 @@ def play():
         loading()
 
         while game_on:
-            marker = player1[0]
+            marker = player1
             display_board(
                 board,
             )
@@ -220,7 +219,7 @@ def play():
             if full_board_check(board):
                 print("The board is full. Nobody won.")
                 break
-            marker = player2[0]
+            marker = player2
             position = player_choice(
                 board,
                 player2,
